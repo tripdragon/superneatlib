654db820a8dcb54fa94c227a486388a5241a2a97
tripdragon <nickitdesign@gmail.com>
2025-01-30 17:04:48 -0500

    khjfggkdfg

--- examples/main.js
+++ examples/main.js
@@ -2,32 +2,16 @@
 // builds a simple cube spinng
 
 
-// import * as THREE from 'three';
-// import { Object3D, Group, BoxGeometry, PointLight } from '/node_modules/three/build/three.module.js';
 import { PointLight } from 'three';
 
-// import * as _s from 'http://localhost:5000/superneatlib.js';
-// import * as _s from './superneatlib.js';
-import { setupPlaneHelper, setupGridHelper, addResizeWindow, Primitives, Lights, init3d, setupOrbitController, setupGameLoopWithFPSClamp } from 'superneatlib';
+
+import { setupPlaneHelper, setupGridHelper,
+  addResizeWindow, Primitives, Lights, init3d,
+  setupOrbitController, setupGameLoopWithFPSClamp } from 'superneatlib';
 // import {CheapPool} from './superneatlib.js';
 
-// need to know how tis references
+// need to know how references
 import { APP as _o } from "superneatlib";
-
-//
-//
-// import { init3d } from '@setups/init3D.js';
-// import { addResizeWindow } from '@setups/addResizeWindow.js';
-// import { setupOrbitController } from '@setups/orbitControls.js';
-//
-//
-// import { addHemisphereLight } from '@setups/addHemisphereLight.js';
-//
-// // import { checkIfFileExists } from '@tools/fileExists.js';
-// import { setupGameLoopWithFPSClamp } from '@setups/gameLoop.js';
-// import { setupPlaneHelper } from '@setups/setupPlaneHelper.js';
-// import { setupGridHelper } from '@setups/setupGridHelper.js';
-//
 
 
 export async function init() {
@@ -38,10 +22,7 @@ export async function init() {
 
   setupGameLoopWithFPSClamp(_o);
 
-  // Resize
   addResizeWindow(_o);
-
-
 
   setupPlaneHelper(_o);
   setupGridHelper({store:_o, type:"y"});
@@ -51,7 +32,6 @@ export async function init() {
   const renderer = _o.renderer;
 
 
-  // Light
   const pointLight = new PointLight(0xffffff, 2, 100);
   pointLight.position.set(5, 5, 5);
   scene.add(pointLight);
@@ -63,7 +43,6 @@ export async function init() {
 
   const ball = Primitives.ball({store:_o, scale: 0.1, color: 0x00ffff});
   ball.position.y += 0.1;
-  // export function ball({store, color = 0xcc44ff, scale = 0.01}={}){
 
 
 
